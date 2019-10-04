980e35ab15b76656e79fb4297c9e0b25dbf9f9ff
a50492002 <43949950+a50492002@users.noreply.github.com>
2019-10-04 13:48:59 +0800

    Update python_seting.sh

--- Raspbian/python_seting.sh
+++ Raspbian/python_seting.sh
@@ -7,7 +7,7 @@ pip3 install --upgrade pip
 sudo sed -i '$a # Add this line inside ~/.bash_profile:' ~/.bashrc
 sudo sed -i '$a export PATH=$PATH:~/.local/bin' ~/.bashrc
 source ~/.bashrc
-
+sudo apt-get install libatlas-base-dev
 
 pip3 install jupyter --user
 jupyter notebook --generate-config
