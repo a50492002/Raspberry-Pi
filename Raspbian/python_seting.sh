a49191a243d375455860f0285211b271f76327c8
a50492002 <43949950+a50492002@users.noreply.github.com>
2019-10-04 11:15:45 +0800

    Create python_seting.sh

---
+++ Raspbian/python_seting.sh
@@ -0,0 +1,15 @@
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#install pip3
+sudo apt-get install python3-pip -y
+pip3 install --upgrade pip
+sudo sed -i '$a # Add this line inside ~/.bash_profile:' ~/.bashrc
+sudo sed -i '$a export PATH=$PATH:~/.local/bin' ~/.bashrc
+source ~/.bashrc
+
+
+pip3 install jupyter --user
+jupyter notebook --generate-config
+jupyter notebook password
+jupyter notebook --ip=0.0.0.0 --port=8888
