825559fc9d3134c8f9e27d73c8323c2edfcce739
a50492002 <43949950+a50492002@users.noreply.github.com>
2019-10-04 10:15:52 +0800

    Update LAMP.sh

--- Raspbian/LAMP.sh
+++ Raspbian/LAMP.sh
@@ -1,2 +1,11 @@
-sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get update -y
+sudo apt-get upgrade -y
+
+#install LAMP
+sudo apt-get install -y apache2
+sudo apt-get install -y mariadb-server mariadb-client
+sudo apt-get install -y php php-cgi libapache2-mod-php php-common php-pear php-mbstring php-mysql
+#sudo a2enconf php7.2-cgi
+sudo a2enconf php7.3-cgi
+sudo systemctl reload apache2
+sudo apt-get install -y phpmyadmin php-mbstring php-gettext
